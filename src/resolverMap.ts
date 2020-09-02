fcfb21ad082e1f38ebf0e335444044b396f61cd0
Larson Carter <larsoncarter@blazermail.net>
2020-09-01 19:45:10 -0500

    GraphQL: Basic Test QL
    I added a few basic Query resolvers to allow me to test the passing of data.

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,9 +1,36 @@
 import { IResolvers } from 'graphql-tools';
+import { prependOnceListener } from 'process';
+
 const resolverMap: IResolvers = {
+
   Query: {
+
     helloWorld(_: void, args: void): string {
-  return `Howdy!`;
+
+      return `Hello, World!`;
+
     },
+
+    people: () => people,
+
   },
+
 };
+
+const people = [
+  {
+    firstName: 'Larson',
+    lastName: 'Carter',
+    city: 'Jonesboro',
+    state: 'Arkansas',
+
+  },
+  {
+    firstName: 'Bubba',
+    lastName: 'Carter',
+    city: 'New Orleans',
+    state: 'Louisiana',
+  },
+]
+
 export default resolverMap;
